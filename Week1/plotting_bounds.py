0409379018d423ab6b805f25473e611ba31b414f
ajders <ajpofficial@gmail.com>
2020-09-03 08:56:44 +0200

    Updated bound comparison plots

--- Week1/plotting_bounds.py
+++ Week1/plotting_bounds.py
@@ -3,12 +3,13 @@ import pdb
 import numpy as np
 import matplotlib.pyplot as plt
 
-def optimal_kl(bias, bound, epsilon):
+def optimal_kl(bias, bound, epsilon, lower):
     '''
     Written by Anders Jess Pedersen, translated from MATLAB implementation by
-    Yevgeny Seldin. Calculates the maximal bias `result` of a Bernoulli variable
-    such that its KL-divergence from a Bernoulli variable with bias `bias` is bounded by
-    `bound`, i.e. `result = argmax_y Dkl(bias||result) < bound`.
+    Yevgeny Seldin. Calculates the maximal bias `result` via binary search of 
+    a Bernoulli variable such that its KL-divergence from a Bernoulli variable
+    with bias `bias` is bounded by `bound`, i.e.
+    `result = argmax_y Dkl(bias||result) < bound`.
 
     Parameters: 
         bias (float): Bias of the Bernoulli variable.
@@ -25,7 +26,7 @@ def optimal_kl(bias, bound, epsilon):
         return bias
 
     result = (1 + bias) / 2
-    step = (1 + bias) / 4
+    step = (1 - bias) / 4
 
     if (bias > 0.0):
         p0 = bias
@@ -33,16 +34,18 @@ def optimal_kl(bias, bound, epsilon):
         p0 = 1.0
     
     while (step > epsilon):
-        result = min(1.0-epsilon, result)
-        tmp = (1 - bias) / (1 - result)
-        below_bound = (
-            (bias * math.log(p0 / result) + (1.0 - bias) * math.log(tmp)) < bound
-        )
-
-        if below_bound:
-            result += step
+        result = min(1.0 - epsilon, result)
+        kl = (bias * math.log(p0 / result) + (1.0 - bias) * math.log((1.0 - bias) / (1.0 - result)))
+        if not lower:
+            if kl < bound:
+                result += step
+            else:
+                result -= step
         else:
-            result -= step
+            if kl > bound:
+                result -= step
+            else:
+                result += step
         step = step / 2
     return result
 
@@ -56,10 +59,7 @@ def hoeffding_bound(phats, delta, n, lower):
 
 def kl_bound(phats, delta, n, lower, epsilon):
     bound_for_kl = math.log((n + 1) / delta) / n
-    if not lower:
-        bounds = np.array([optimal_kl(phat, bound_for_kl, epsilon) for phat in phats])
-    else:
-        bounds = np.array([optimal_kl(phat, bound_for_kl, epsilon) for phat in phats]) # DO OTHER CALCULATION HERE
+    bounds = np.array([optimal_kl(phat, bound_for_kl, epsilon, lower) for phat in phats])
     return zip(phats, bounds)
 
 
@@ -72,16 +72,16 @@ def refined_pinsker_bound(phats, delta, n):
         + (2 * math.log((n + 1) / delta)) / n
     return zip(phats, bounds)
 
-def remove_too_large(zipped_data):
-    return [(x, y) for (x, y) in zipped_data if y < 1.0]
+def remove_out_of_bounds(zipped_data):
+    return [(x, y) for (x, y) in zipped_data if y < 1.0 and y > 0.0]
 
 def make_plot(n = 1000, delta = 0.01, interval=[0.0, 1.0], lower=False, epsilon=2**(-52)):
     step_size = interval[1] / 1000
     phats = np.array([interval[0]+step_size*x for x in range(1000)])
-    hoef_phats, hoef_bounds = zip(*remove_too_large(hoeffding_bound(phats, delta, n, lower)))
-    kl_phats, kl_bounds = zip(*remove_too_large(kl_bound(phats, delta, n, lower, epsilon)))
-    pin_phats, pin_bound = zip(*remove_too_large(pinsker_bound(phats, delta, n)))
-    ref_pin_phats, ref_pin_bound = zip(*remove_too_large(refined_pinsker_bound(phats, delta, n)))
+    hoef_phats, hoef_bounds = zip(*remove_out_of_bounds(hoeffding_bound(phats, delta, n, lower)))
+    kl_phats, kl_bounds = zip(*remove_out_of_bounds(kl_bound(phats, delta, n, lower, epsilon)))
+    pin_phats, pin_bound = zip(*remove_out_of_bounds(pinsker_bound(phats, delta, n)))
+    ref_pin_phats, ref_pin_bound = zip(*remove_out_of_bounds(refined_pinsker_bound(phats, delta, n)))
 
     _, ax = plt.subplots()
     ax.plot(hoef_phats, hoef_bounds, label='Hoeffding')
